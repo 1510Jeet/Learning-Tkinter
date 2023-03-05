2b941b8174249077524cabd3f9028501265f5828
1510Jeet <1510jeet@gmail.com>
2023-03-05 10:54:27 +0530

    Image in a gui-tkinter app.

--- new.py
+++ new.py
@@ -1,5 +1,17 @@
 from tkinter import *
+from PIL import ImageTk,Image
 root=Tk()
 root.title('Learning Tkinter')
 root.iconbitmap(r"C:\Users\Jeet\Downloads\python_icon_245273.ico") # for adding icon
-root.mainloop()
+
+my_img=ImageTk.PhotoImage(Image.open('mz.png'))
+my_label=Label(image=my_img)
+my_label.grid()
+
+
+button_quit=Button(root,text="Exit Program", command=root.quit)
+button_quit.grid()
+
+
+
+root.mainloop() 
